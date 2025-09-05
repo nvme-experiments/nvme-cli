4254c148335f2009d66a35e783aa3270b83d33b3
Tokunori Ikegami <ikegami.t@gmail.com>
2025-09-05 16:50:42 +0900

    nvme: nvme_security_receive use nvme_passthru_cmd directly
    nvme_security_receive_args dropped in libnvme.
    
    Signed-off-by: Tokunori Ikegami <ikegami.t@gmail.com>
    [wagi: reorder arguments matchig the API update]
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- nvme-rpmb.c
+++ nvme-rpmb.c
@@ -292,25 +292,11 @@ static int send_rpmb_req(nvme_link_t l, unsigned char tgt, int size,
 	return nvme_security_send(l, &args);
 }
 
-static int recv_rpmb_rsp(nvme_link_t l, int tgt, int size,
-			 struct rpmb_data_frame_t *rsp)
+static int recv_rpmb_rsp(nvme_link_t l, int tgt, int size, struct rpmb_data_frame_t *rsp)
 {
-
-	struct nvme_security_receive_args args = {
-		.args_size	= sizeof(args),
-		.nsid		= 0,
-		.nssf		= tgt,
-		.spsp0		= RPMB_NVME_SPSP,
-		.spsp1		= 0,
-		.secp		= RPMB_NVME_SECP,
-		.al		= 0,
-		.data_len	= size,
-		.data		= (void *)rsp,
-		.timeout	= NVME_DEFAULT_IOCTL_TIMEOUT,
-		.result		= NULL,
-	};
-
-	return nvme_security_receive(l, &args);
+	return nvme_security_receive(l, 0, tgt,
+				     RPMB_NVME_SPSP, RPMB_NVME_SECP, 0,
+				     (void *)rsp, size, NULL);
 }
 
 /* Initialize nonce value in rpmb request frame */
